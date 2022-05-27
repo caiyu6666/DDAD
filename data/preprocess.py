6387626b72153a2ef2e1c568d92a7251904a7cc5
caiyu6666 <caiyu@hust.edu.cn>
2022-05-27 08:37:32 +0800

    update

--- data/preprocess.py
+++ data/preprocess.py
@@ -8,17 +8,19 @@ in_dir = "stage_2_train_images"  # Your path to original data (.dcm or .dicom)
 out_dir = "train_png_512"  # Your output path for preprocessed data (.png)
 img_size = 512  # image size of png
 
-if not os.path.exists(out_dir):
-	os.mkdir(out_dir)
 
-for img_name in tqdm(os.listdir(in_dir)):
-	dicom = pydicom.read_file(os.path.join(in_dir, img_name))
-	img = dicom.pixel_array
-	if dicom.PhotometricInterpretation == "MONOCHROME1":
-		img = np.max(img) - img
+if __name__ == "__main__":
+	if not os.path.exists(out_dir):
+		os.mkdir(out_dir)
 
-	if img.dtype != np.uint8:
-		img = ((img - np.min(img)) * 1.0 / (np.max(img) - np.min(img)) * 255).astype(np.uint8)
+	for img_name in tqdm(os.listdir(in_dir)):
+		dicom = pydicom.read_file(os.path.join(in_dir, img_name))
+		img = dicom.pixel_array
+		if dicom.PhotometricInterpretation == "MONOCHROME1":
+			img = np.max(img) - img
 
-	img = Image.fromarray(img).convert('L').resize((img_size, img_size), resample=Image.BILINEAR)
-	img.save(os.path.join(out_dir, img_name.split(".")[0] + ".png"))
+		if img.dtype != np.uint8:
+			img = ((img - np.min(img)) * 1.0 / (np.max(img) - np.min(img)) * 255).astype(np.uint8)
+
+		img = Image.fromarray(img).convert('L').resize((img_size, img_size), resample=Image.BILINEAR)
+		img.save(os.path.join(out_dir, img_name.split(".")[0] + ".png"))
