09b1c51977aba9a9446f1ebb6368c540cd066f77
caiyu6666 <caiyu@hust.edu.cn>
2022-06-12 11:43:44 +0800

    parallel loading

--- anomaly_data.py
+++ anomaly_data.py
@@ -29,42 +29,13 @@ class AnomalyDetectionDataset(data.Dataset):
 
         print("Loading images")
         if mode == "train":
-            # t0 = time.time()
             train_normal = data_dict["train"]["0"]
-            # self.slices += Parallel(n_jobs=-1, verbose=1)(delayed(
-            #     lambda file: Image.open(os.path.join(self.root, "train_png_512", file)).convert("L").resize(
-            #         (img_size, img_size), resample=Image.BILINEAR))(file) for file in train_normal)
-            # self.labels += len(train_normal) * [0]
-            # self.img_id += [img_name.split('.')[0] for img_name in train_normal]
-            # print("Loaded {} normal images. {:.3f}s".format(len(train_normal), time.time()-t0))
-            # for img_name in tqdm(train_normal):
-            #     img = Image.open(os.path.join(self.root, "train_png_512", img_name)).convert('L').resize(
-            #         (img_size, img_size), resample=Image.BILINEAR)
-            #     self.slices.append(img)
-            #     self.labels.append(0)
-            #     self.img_id.append(img_name.split('.')[0])
 
             normal_l = data_dict["train"]["unlabeled"]["0"]
             abnormal_l = data_dict["train"]["unlabeled"]["1"]
             if extra_data > 0:
                 abnormal_num = int(extra_data * ar)
                 normal_num = extra_data - abnormal_num
-
-                # for img_name in tqdm(normal_l[:normal_num]):
-                #     img = Image.open(os.path.join(self.root, "train_png_512", img_name)).convert('L').resize(
-                #         (img_size, img_size), resample=Image.BILINEAR)
-                #
-                #     self.slices.append(img)
-                #     self.labels.append(0)
-                #     self.img_id.append(img_name.split('.')[0])
-
-                # for img_name in tqdm(abnormal_l[:abnormal_num]):
-                #     img = Image.open(os.path.join(self.root, "train_png_512", img_name)).convert('L').resize(
-                #         (img_size, img_size), resample=Image.BILINEAR)
-                #
-                #     self.slices.append(img)
-                #     self.labels.append(1)
-                #     self.img_id.append(img_name.split('.')[0])
             else:
                 abnormal_num = 0
                 normal_num = 0
@@ -90,38 +61,6 @@ class AnomalyDetectionDataset(data.Dataset):
             self.img_id += [img_name.split('.')[0] for img_name in test_l]
             print("Loaded {} test normal images, "
                   "{} test abnormal images. {:.3f}s".format(len(test_normal), len(test_abnormal), time.time() - t0))
-
-            # test normal
-            # t0 = time.time()
-            # self.slices += Parallel(n_jobs=10)(delayed(
-            #     lambda file: Image.open(os.path.join(self.root, "train_png_512", file)).convert("L").resize(
-            #         (img_size, img_size), resample=Image.BILINEAR))(file) for file in test_normal)
-            # self.labels += len(test_normal) * [0]
-            # self.img_id += [img_name.split('.')[0] for img_name in test_normal]
-            # print("Loaded {} test normal images. {:.3f}s".format(len(test_normal), time.time() - t0))
-
-            # for img_name in tqdm(test_normal):
-            #     img = Image.open(os.path.join(self.root, "train_png_512", img_name)).convert('L').resize(
-            #         (img_size, img_size), resample=Image.BILINEAR)
-            #     self.slices.append(img)
-            #     self.labels.append(0)
-            #     self.img_id.append(img_name.split('.')[0])
-
-            # test abnormal
-            # t0 = time.time()
-            # self.slices += Parallel(n_jobs=10)(delayed(
-            #     lambda file: Image.open(os.path.join(self.root, "train_png_512", file)).convert("L").resize(
-            #         (img_size, img_size), resample=Image.BILINEAR))(file) for file in test_abnormal)
-            # self.labels += len(test_abnormal) * [0]
-            # self.img_id += [img_name.split('.')[0] for img_name in test_abnormal]
-            # print("Loaded {} test abnormal images. {:.3f}s".format(len(test_abnormal), time.time() - t0))
-
-            # for img_name in tqdm(test_abnormal):
-            #     img = Image.open(os.path.join(self.root, "train_png_512", img_name)).convert('L').resize(
-            #         (img_size, img_size), resample=Image.BILINEAR)
-            #     self.slices.append(img)
-            #     self.labels.append(1)
-            #     self.img_id.append(img_name.split('.')[0])
 
     def __getitem__(self, index):
         img = self.slices[index]
